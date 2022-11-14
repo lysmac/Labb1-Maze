2760f440c1c4512be5861ca19ae789561c082388
lysmac <calle@lysmask.se>
2022-11-14 13:15:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const mymaze = [
+  { spot: 1, east: "true", south: "true" },
+  { spot: 2, east: "true", south: "true", west: "true" },
+  { spot: 3, north: "true", west: "true" },
+  { spot: 4, east: "true", south: "true" },
+  { spot: 5, west: "true" },
+
+  { spot: 6, north: "true", south: "true" },
+  { spot: 7, north: "true", east: "true" },
+  { spot: 8, east: "true", west: "true" },
+  { spot: 9, north: "true", east: "true", west: "true" },
+  { spot: 10, south: "true", west: "true" },
+
+  { spot: 11, north: "true", east: "true" },
+  { spot: 12, west: "true" },
+  { spot: 13, east: "true", south: "true" },
+  { spot: 14, east: "true", west: "true" },
+  { spot: 15, north: "true", south: "true", west: "true" },
+
+  { spot: 16, east: "true", south: "true" },
+  { spot: 17, east: "true", west: "true" },
+  { spot: 18, north: "true", west: "true" },
+  { spot: 19, south: "true" },
+  { spot: 20, north: "true", south: "true" },
+
+  { spot: 21, north: "true", east: "true" },
+  { spot: 22, east: "true", west: "true" },
+  { spot: 23, west: "true" },
+  { spot: 24, north: "true", east: "true" },
+  { spot: 25, north: "true", west: "true" },
+];
